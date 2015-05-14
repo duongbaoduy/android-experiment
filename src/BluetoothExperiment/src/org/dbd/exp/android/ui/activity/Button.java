8a9feb161f6d91129c31269bafbf2b90825ded3d
Duong Bao Duy <baoduy.duong0206@gmail.com>
2015-05-14 14:41:11 +0700

    - add bluetooth example
    Signed-off-by: Duong Bao Duy <baoduy.duong0206@gmail.com>

---
+++ src/BluetoothExperiment/src/org/dbd/exp/android/ui/activity/Button.java
@@ -0,0 +1,12 @@
+package org.dbd.exp.android.ui.activity;
+
+import org.dbd.exp.android.ui.BaseActivity;
+
+import android.os.Bundle;
+
+public class Button extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
